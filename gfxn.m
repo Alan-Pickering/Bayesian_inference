7760d2b927c3e7ae0a25bfc23664a5b083687f77
Alan-Pickering <57485507+Alan-Pickering@users.noreply.github.com>
2023-04-13 00:39:31 +0900

    Add files via upload

--- gfxn.m
+++ gfxn.m
@@ -1,16 +1,31 @@
-function gout = gfxn(gin, M, choice)
+function gout = gfxn(gin, ulimits, choice)
 
+s=size(ulimits,1);
+if s>0
+   Mx=ulimits(1);
+end
+if s==2
+   Mn=ulimits(2);
+end
 if choice==-1
-    %meanu_fromv= (MAXV/(MAXV+1)).*(MAXV + 1 - gfxn(vrange,fxntype));
-    gout = (M/(M+1)).*(M + 1 - gin);
+    gout = (Mx/(Mx+1)).*(Mx + 1 - gin);
+elseif choice==-3
+    gout = Mx + 1 - gin;
 elseif choice==1
     gout = gin; %a simple linear function
 elseif choice==2
     gout = gin.^2; %non-linear function used in Bogacz example
 elseif choice==3
-    gout=M./gin;
+    gout=Mx./gin;
 else
     %@@@ add other function choices
+end
+
+%preserving the min value of u
+if s==2
+  gout=max(gout,Mn);
+  %testgo=gout<Mn;
+  %disp(sum(testgo));
 end
 
 endfunction
